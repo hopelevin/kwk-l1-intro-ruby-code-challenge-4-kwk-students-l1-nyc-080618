c1d009f740094f0353a9d65d606f3eb89a08e53e
hopelevin <hopealevin@gmail.com>
2018-08-09 17:02:16 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
-#write out your code here
+def least_coins 
+coins = ["Quarters" , "Dimes" , "Nickels" , "Pennies"]
+value = [0.25 , 0.10 , 0.05 , 0.01]
 
-def least_coins(cents)
-
-#Code your answer here!
-
-end
+coins.each do |money|
+  coins_hash[money] = value[counter]
+  counter += 1
